c883968e761c945e0e0b00e3db07090558ba6ad6
maksimsgoldins <maksims.goldins@spes.lv>
2026-04-15 16:56:23 +0300

    Add new model imports to __init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -4,3 +4,10 @@ from app.models.attribute import Attribute, AttributePossibleValue
 from app.models.attribute_involvement import AttributeInvolvement, AttributeInvolvementAllowedValue, AttributeInvolvementDefaultValue
 from app.models.diagram_layout import DiagramLayout
 from app.models.attribute_propagation import AttributePropagation
+from .task_spec import TaskSpec
+from .gateway_spec import GatewaySpec
+from .event_spec import EventSpec
+from .process_spec import ProcessSpec
+from .process_element import ProcessElement
+from .process_flow import ProcessFlow
+from .inter_process_dependency import InterProcessTaskDependency
