403d0c0c1e9fb04a6a8764a7bb02ba796fe2f303
maksimsgoldins <maksims.goldins@spes.lv>
2026-04-15 16:56:59 +0300

    Add multiple routers for task and process specs

--- app/main.py
+++ app/main.py
@@ -2,6 +2,13 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.router import api_router
 from app.core.config import settings
+from app.api.routes.task_specs import router as task_specs_router
+from app.api.routes.gateway_specs import router as gateway_specs_router
+from app.api.routes.event_specs import router as event_specs_router
+from app.api.routes.process_specs import router as process_specs_router
+from app.api.routes.process_elements import router as process_elements_router
+from app.api.routes.process_flows import router as process_flows_router
+from app.api.routes.inter_process_dependencies import router as inter_process_dependencies_router
 
 app = FastAPI(title=settings.app_name)
 
